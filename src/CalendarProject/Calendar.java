c202f693170dd9e1ad18e6d868f4cef84363a131
hyunzoo <guswnrla99@naver.com>
2020-03-29 00:23:44 +0900

    separate month max day

--- src/CalendarProject/Calendar.java
+++ src/CalendarProject/Calendar.java
@@ -9,10 +9,6 @@ public class Calendar {
 		String PROMPT = "cal> ";
 		Scanner scanner = new Scanner(System.in);
 		sub show = new sub();
-		show.printSampleCalendar();
-
-		System.out.println("반복횟수를 입력하세요.");
-
 		int month = 1;
 
 		while (true) {
@@ -25,7 +21,7 @@ public class Calendar {
 			if(month > 12) {
 				continue;
 			}
-			System.out.println(month + "월은 " + show.getMaxDayOfMonth(month) + "일까지 있습니다.");
+			show.printSampleCalendar(month);
 		}
 
 		System.out.println("done");
--- src/CalendarProject/sub.java
+++ src/CalendarProject/sub.java
@@ -1,19 +1,38 @@
 package CalendarProject;
 
 public class sub {
-	
-	final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
-	public void printSampleCalendar() {
-		
-		System.out.println("일    월     화   수    목    금    토");
-		System.out.println("---------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+
+	final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public void printSampleCalendar(int month) {
+
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			System.out.println("일    월     화   수    목    금    토");
+			System.out.println("---------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30 31");
+		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			System.out.println("일    월     화   수    목    금    토");
+			System.out.println("---------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30");
+		} else {
+			System.out.println("일    월     화   수    목    금    토");
+			System.out.println("---------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+
+		}
 	}
-	
+
 	public int getMaxDayOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 
