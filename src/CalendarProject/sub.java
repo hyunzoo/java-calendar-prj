78f68e74322f540cfcb9d30165eabeb51f82bd53
hyunzoo <guswnrla99@naver.com>
2020-03-28 23:33:21 +0900

    update using class method

---
+++ src/CalendarProject/sub.java
@@ -0,0 +1,22 @@
+package CalendarProject;
+
+public class sub {
+	
+	final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public void printSampleCalendar() {
+		
+		System.out.println("일    월     화   수    목    금    토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+	
+	public int getMaxDayOfMonth(int month) {
+		return MAX_DAYS[month - 1];
+
+	}
+
+}
