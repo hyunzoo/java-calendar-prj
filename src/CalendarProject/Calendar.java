62a24ed70646a3442028170e9234747e28ca991d
hyunzoo <guswnrla99@naver.com>
2020-03-29 20:20:37 +0900

    for문과 if문을 이용한 달력생성
    *if(i % 7 == 0 )

--- src/CalendarProject/Calendar.java
+++ src/CalendarProject/Calendar.java
@@ -6,27 +6,6 @@ public class Calendar {
 
 	public static void main(String[] args) {
 
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		sub show = new sub();
-		int month = 1;
-
-		while (true) {
-			System.out.println("달을 입력하세요.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1) {
-				break;
-			}
-			if(month > 12) {
-				continue;
-			}
-			show.printSampleCalendar(month);
-		}
-
-		System.out.println("done");
-
-		scanner.close();
 	}
 
 }
